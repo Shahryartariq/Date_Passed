062f4e0090e07410f9cb6e0c23717446c93d03b3
Sherry <sherry@ameeqcode.com>
2024-09-27 11:53:45 +0500

    Friday Morning

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
     <!-- Display the Active Time Counter -->
     <div v-if="activeTab === 0" class="tab-content">
       <h2>LTM</h2>
-      <TimeCounter :startDate="new Date('2024-09-24T08:58:00')" />
+      <TimeCounter :startDate="new Date('2024-09-27T10:44:00')" />
     </div>
     <div v-if="activeTab === 1" class="tab-content">
       <h2>LTC</h2>
