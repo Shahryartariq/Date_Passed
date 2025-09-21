23c79cb2617391ab92e3e25281285fd95616598e
Muhammad Shehryar <Shahryartariq57@gmail.com>
2025-09-22 02:15:24 +0500

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Date_Passed/',
   plugins: [vue()],
+  build: {
+    outDir: 'dist'
+  }
 })
