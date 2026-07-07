c7653ec08b643380153509547add2ac5af078048
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-07-07 08:36:59 +0500

    updated on tuesday

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
     <!-- Display the Active Time Counter -->
     <div v-if="activeTab === 0" class="tab-content">
       <h2>LTM</h2>
-      <TimeCounter :startDate="new Date('2026-07-06T08:03:00')" />
+      <TimeCounter :startDate="new Date('2026-07-07T08:06:00')" />
     </div>
     <div v-if="activeTab === 1" class="tab-content">
       <h2>LTC</h2>
